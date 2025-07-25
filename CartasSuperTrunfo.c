9e6182c96bfd127e5541a9fd57362de08161d3cc
liosnake <juliocezarrsouza@gmail.com>
2025-07-25 00:31:53 +0000

    cadastro e a exibição de duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,75 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
+
+    printf("\nSuper Trunfo - Jogo de Cartas\n");
+
+    printf("\ncarta 1\n");
+
+    char Estado [50] = "São Paulo";
+    printf("Estado: %s\n", Estado);
+
+    char Codigo[50] = "A01";
+    printf("Codigo: %s\n", Codigo);
+
+    char Cidade[50] = "São Paulo";
+    printf("Cidade: %s\n", Cidade);
+
+    int População = 12325000;
+    printf("População: %d\n", População);
+
+    float Area = 1521.11;
+    printf("Area: %.2f km²\n", Area);
+
+    float PIB = 699.28;
+    printf("PIB: %.2f bilhões\n", PIB);
+
+    int pontosturisticos = 50;
+    printf("pontos turisticos: %d\n", pontosturisticos);
+
+
+
+
+    // Informações da segunda carta
+
+    
+
+
+    printf("\ncarta 2\n");
+
+    char Estado2 [50] = "Rio de Janeiro";
+    printf("Estado: %s\n", Estado2);
+
+    char Codigo2[50] = "B02";
+    printf("Codigo: %s\n", Codigo2);
+
+    char Cidade2[50] = "Rio de Janeiro";
+    printf("Cidade: %s\n", Cidade2);
+
+    int População2 = 6748000;
+    printf("População: %d\n", População2);
+
+    float Area2 = 1200.25;
+    printf("Area: %.2f km²\n", Area2);
+
+    float PIB2 = 300.50;
+    printf("PIB: %.2f bilhões\n", PIB2);
+
+    int pontosturisticos2 = 30;
+    printf("pontos turisticos: %d\n", pontosturisticos2);
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
